ac6479ac488b5f37ea4ec019b770623373efa6e5
miguecoder <mmarqzm@gmail.com>
2022-09-17 18:12:15 -0500

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Python script that give Status of API"""
+
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def Status():
+    """returns a json with the status"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def counter():
+    """retrieves the number of each objects by type"""
+    dict_return = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+
+    return jsonify(dict_return)
